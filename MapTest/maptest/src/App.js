cb4847efa078c7bb13b32f79f9ae67ae9193ad88
Paul <clas.paul@gmail.com>
2021-03-07 23:27:50 -0500

    Plotter

---
+++ MapTest/maptest/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LinearGraph from './Map/LinearGraph.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <LinearGraph/>
+    </div>
+  );
+}
+
+export default App;
